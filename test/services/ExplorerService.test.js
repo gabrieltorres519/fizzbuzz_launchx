a04ab71f3644f79837fbcf6b41ddd3fdb8c5098b
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-05-01 14:06:59 -0500

    Creadas y pasadas las pruebas unitarias de la clase ExplorerService

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,13 +1,23 @@
 const ExplorerService = require("../../lib/services/ExplorerService");
+const Reader = require("../../lib/utils/Reader")
 
 
 describe("Unit tests for ExplorerService class",()=>{
 
-    test("1) ",()=>{
+    test("1) Filter explorers by mission, get amount and usernames ",()=>{
 
-        const a =1;
+        //Código que se usa en la app
+        const explorers = Reader.readJsonFile("explorers.json"); 
 
-        expect(a).toBe(1)
+        const explorersByMission = ExplorerService.filterByMission(explorers, "node");
+        const amountOfExplorersByMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        const usernamesOfExplorersByMission = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+
+        
+        expect(explorersByMission[0].mission).toBe("node")
+        expect(amountOfExplorersByMission).toBe(10)
+        expect(usernamesOfExplorersByMission[0]).toBe("ajolonauta1")
+        
     })
 
 })
